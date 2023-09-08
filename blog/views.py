b4f4709f4baf5fea9e3801dd7e767139c475a81a
pikakip1 <pikakip222@mail.ru>
2023-09-08 15:08:31 +0300

    show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
@@ -27,9 +28,15 @@ def serialize_tag(tag):
     }
 
 
+def get_likes_count(post):
+    return post.likes__count
+
+
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.annotate(Count('likes'))
+
+    most_popular_posts = sorted(posts, key=get_likes_count)[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
