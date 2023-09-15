811c4b9acc56c74f92f5c05e088154b9ca4876d2
pikakip1 <pikakip222@mail.ru>
2023-09-15 21:11:54 +0300

    reduce db queries number.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
@@ -15,7 +16,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -29,7 +30,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -37,7 +38,7 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': Post.objects.filter(tags=tag).count(),
+        'posts_with_tag': tag.posts__count,
     }
 
 
@@ -67,7 +68,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.all().annotate(Count('posts'))
 
     serialized_post = {
         'title': post.title,
@@ -102,7 +103,7 @@ def tag_filter(request, tag_title):
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
-    related_posts = tag.posts.all()[:20]
+    related_posts = tag.posts.all().annotate(Count('posts'))[:20]
 
     context = {
         'tag': tag.title,
